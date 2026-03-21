c2fd46f949515e5539b562abec265edc47602076
fj <fj@somedomain.com>
2026-03-20 21:40:16 -0700

    fixed bug in debugger script.

--- conf_captured/.scripts/debugger.sh
+++ conf_captured/.scripts/debugger.sh
@@ -6,9 +6,9 @@
 current_pane=$(tmux display-message -p "#{pane_id}")
 
 # check if a tmux pane with title "debugger" already exists
-existing_debug_pane=$(tmux list-panes -F "#{pane_id} #{pane_title}" | grep " debugger$" | awk '{print $1}')
+existing_pane=$(tmux list-panes -F "#{pane_id} #{pane_title}" | grep " debugger$" | awk '{print $1}')
 
-if [ -n "$existing_debug_pane" ]; then
+if [ -n "$existing_pane" ]; then
     #tmux select-pane -t "$existing_pane"
     #tmux send-keys -t "$existing_pane" "./debug.sh" C-m
     tmux send-keys -t "$existing_pane" "./debug.sh" C-m
@@ -24,14 +24,14 @@ else
 
     # set the title using the standard escape sequence (no select-pane needed).
     # ] means operating system command. 2 is rename.
-    #tmux send-keys -t "$new_pane" "printf '\033]2;debugger\033\\'" C-m
+    tmux send-keys -t "$new_pane" "printf '\033]2;debugger\033\\'" C-m
 
     # set tmux pane title using a user option @title.
     #tmux set-option -t "$new_pane" -p @title "debugger"
 
     
-    tmux select-pane -t "$new_pane" -T "debugger"
-    tmux select-pane -t "$current_pane"
+    #tmux select-pane -t "$new_pane" -T "debugger"
+    #tmux select-pane -t "$current_pane"
 
     tmux send-keys -t "$new_pane" "./debug.sh" C-m
 fi
